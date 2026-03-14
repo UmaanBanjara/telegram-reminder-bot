f2576e215d6e619f0c8cb961e436fe7ec025f981
Umaan Banjara <umaanbanjara@gmail.com>
2026-03-14 11:53:52 +0545

    main python file

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.app_routes import router  
+
+app = FastAPI(title='RemindMe')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router, prefix='/api', tags=['API'])
+
+@app.get('/')
+def root():
+    return {
+        'message': 'Welcome to RemindMe Bot'
+    }
