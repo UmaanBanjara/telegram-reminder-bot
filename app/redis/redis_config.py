def5d7ec7a6c3c044b3ffddf96ca22a29166e54a
Umaan Banjara <umaanbanjara@gmail.com>
2026-03-14 17:06:17 +0545

    redis and slow api config

--- app/redis/redis_config.py
+++ app/redis/redis_config.py
@@ -3,14 +3,19 @@ from slowapi import Limiter
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.util import get_remote_address
 import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+
+REDIS_URL = os.getenv("REDIS_URL","redis://localhost:6379/0")
 #connect redis client
-redis_client = redis.Redis(host="localhost",port=6379 , db=0)
+redis_client = redis.Redis.from_url(REDIS_URL)
 
 #create limiter to limit ip
 limiter = Limiter(
     key_func=get_remote_address, #determines ip
-    storage_uri="redis://localhost:6379/0" #stores data in redis db 0
+    storage_uri=REDIS_URL #stores data in redis db 0
 )
 
 #export limiter and put it in main.py to access teh slow api middleware globally
